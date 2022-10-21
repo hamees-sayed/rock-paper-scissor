47b399ff7d1d4aff112fb0151b5624f28035e480
Hamees Sayed <hameessayed71@gmail.com>
2022-10-21 23:52:24 +0530

    added DOM and functionalities

--- script/index.js
+++ script/index.js
@@ -9,7 +9,7 @@ const computer = document.querySelector('.computer');
 const player = [rock, paper, scissor]
 let system = ["rock", "paper", "scissor"];
 let move = 0;
-let yourScore = 0;
+let yourScore= 0;
 let computerScore = 0;
 
 
@@ -19,34 +19,42 @@ const playerChoice = player.forEach(option => {
         moves.textContent = `Moves: ${move}`;
     })
 });
-
-// function computerChoice() {
-//     let system = [rock, paper, scissor];
-//     return Math.floor(Math.random() * system.length)
-// };  
+ 
 
 function resetGame() {
     move = 0;
+    computerScore = 0;
+    yourScore = 0;
+    computer.textContent = `Computer: ${computerScore}`;
+    you.textContent = `You: ${yourScore}`;
     moves.textContent = `Moves: ${move}`;
+    console.log("reset")
 };
 
 reset.addEventListener('click', resetGame);   
 
+
 rock.addEventListener('click', () => {
 
     const i = Math.floor(Math.random() * system.length)
-    
-
 
     if (i === 0) {
         console.log("tie")
         console.log(system[i])
+        computerScore += 1;
+        yourScore += 1;
+        computer.textContent = `Computer: ${computerScore}`;
+        you.textContent = `You: ${yourScore}`;
     } else if (i === 1) {
         console.log("computer wins")
         console.log(system[i])
-    } else if (i === 2) {
+        computerScore += 1;
+        computer.textContent = `Computer: ${computerScore}`;
+    } else {
         console.log("you win")
         console.log(system[i])
+        yourScore += 1;
+        you.textContent = `You: ${yourScore}`;
     }
 });
 
@@ -54,37 +62,46 @@ paper.addEventListener('click', () => {
 
     const i = Math.floor(Math.random() * system.length)
 
-
     if (i === 0) {
         console.log("you win")
         console.log(system[i])
+        yourScore += 1;
+        you.textContent = `You: ${yourScore}`;
     } else if (i === 1) {
         console.log("tie")
         console.log(system[i])
-    } else if (i === 2) {
+        computerScore += 1;
+        yourScore += 1;
+        computer.textContent = `Computer: ${computerScore}`;
+        you.textContent = `You: ${yourScore}`;
+    } else {
         console.log("computer wins")
         console.log(system[i])
+        computerScore += 1;
+        computer.textContent = `Computer: ${computerScore}`;
     }
-
 });
 
 scissor.addEventListener('click', () => {
 
     const i = Math.floor(Math.random() * system.length)
 
-
     if (i === 0) {
         console.log("computer wins")
         console.log(system[i])
+        computerScore += 1;
+        computer.textContent = `Computer: ${computerScore}`;
     } else if (i === 1) {
         console.log("you win")
         console.log(system[i])
-    } else if (i === 2) {
+        yourScore += 1;
+        you.textContent = `You: ${yourScore}`;
+    } else {
         console.log("tie")
         console.log(system[i])
+        computerScore += 1;
+        yourScore += 1;
+        computer.textContent = `Computer: ${computerScore}`;
+        you.textContent = `You: ${yourScore}`;
     }
-    
 });
-
-
-
